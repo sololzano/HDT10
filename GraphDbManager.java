84d4147dfb2f2ecd49b46e1bc0dc66bc49d34e5f
diegolopez96 <lop141222@uvg.edu.gt>
2016-11-09 16:38:15 -0600

    Add files via upload

---
+++ GraphDbManager.java
@@ -0,0 +1,240 @@
+import org.graphstream.algorithm.PageRank;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.ui.view.Viewer;
+
+import javax.swing.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GraphDbManager {
+    /**
+     * Datos de autenticacion de la base de datos remota
+     * 
+     */
+    private static GraphDbManager instance = null;
+    private static final String USER = "hdt_10";
+    private static final String PASSWORD = "CAdWFcoLA9nzzMRQySI5";
+    private static final String CONNECTION = "jdbc:neo4j:bolt://" +
+            "hobby-fkpcdappojekgbkenopbehol.dbs.graphenedb.com:24786";
+    private GraphConnections connections;
+
+    private GraphDbManager(){connections = new GraphConnections(CONNECTION, USER, PASSWORD);}
+
+    /**
+     * Metodo del Multiton para solo obtener una instancia
+     * @return Instancia del DataBase Manager
+     */
+    public static GraphDbManager getInstance(){
+        if (instance == null){
+            instance = new GraphDbManager();
+        }
+        return instance;
+    }
+
+   
+
+    /**
+     * Metodo que muestra el grafo actual de la base de datos
+     */
+    public void dbToGraph(){
+        DbToGraphThread thread = new DbToGraphThread();
+        new Thread(thread).start();
+    }
+
+    /**
+     * Metodo que muestra el Page Rank del grafo de la db
+     */
+    public void showPageRank(){
+        PageRankThread thread = new PageRankThread();
+        new Thread(thread).start();
+    }
+
+    /**
+     * Metodo que muestra el grafo con los nodos que tienen seis o
+     * mas correos enviados
+     */
+    public void sixOrMoreMails(){
+        SixOrMoreThread thread = new SixOrMoreThread();
+        new Thread(thread).start();
+    }
+
+    /**
+     * Metodo que muestra el grafo simplificado
+     */
+    public void simplifyGraph(){
+        SimplifyThread thread = new SimplifyThread();
+        new Thread(thread).start();
+    }
+
+
+   
+
+
+
+
+
+    /**
+     * Clase que devuelve la cantidad minima de correos enviadas a alguna otra persona. Pueden ser directos o indirectos, dependiendo
+     * del query.
+     * @param rs Es el resultado del query de correos directos o indirectos
+     * @param origen es la persona que envia el correo
+     * @return Devuelve el minimo de correos enviados entre dicha persona; Devuelve -1 si no encontro la coincidencia;
+     * @throws SQLException
+     */
+    private int getMinimoCorreosEnviados(ResultSet rs, String origen) throws SQLException {
+        boolean found = false;
+        int resultado = 0;
+        while (rs.next() && !found){
+            if (origen.equalsIgnoreCase((String)rs.getObject("x.name"))){
+                resultado = Math.toIntExact((long) rs.getObject("total"));
+                found = true;
+            }
+        }
+        return resultado;
+    }
+
+
+    /**
+     * Clase que devuelve el total de correos enviados entre dos personas. Pueden ser directos o indirectos, dependiendo
+     * del rs.
+     * @param rs Es el resultado del query de correos directos o indirectos
+     * @param origen es la persona que envia el correo
+     * @param destino es la persona que recive el correo
+     * @return Devuelve el total de correos enviados entre ambas personas; Devuelve -1 si no encontro la coincidencia;
+     * @throws SQLException
+     */
+    private int getTotalCorreosEnviados(ResultSet rs, String origen, String destino) throws SQLException {
+        boolean found = false;
+        String target = origen + destino;
+        int resultado = 0;
+        while (rs.next() && !found){
+            if (target.equalsIgnoreCase((String)rs.getObject("x.name") + (String)rs.getObject("y.name"))){
+                resultado = Math.toIntExact((long) rs.getObject("total"));
+                found = true;
+            }
+        }
+        return resultado;
+    }
+
+
+
+
+    /**
+     * Clase privada, que obtiene de la DB el grafo simplifcado
+     */
+    private class SimplifyThread implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                String query = "MATCH (x:User)-[rel:SEND]->(y:User)\n" +
+                        "WHERE x <> y\n" +
+                        "RETURN rel, x, y";
+                final Graph graph = connections.getAsGraph(query, "Simplified Graph");
+
+                SwingUtilities.invokeLater( () -> {
+                    graph.addAttribute("ui.stylesheet",
+                            "node {fill-color: blue; size-mode: dyn-size;} edge {fill-color:grey;}");
+                    Viewer viewer = graph.display();
+                    viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+                });
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Clase thread que obtiene de la db el grafo con los nodos con mas de 6 correos
+     */
+    private class SixOrMoreThread implements Runnable {
+
+        @Override
+        public void run() {
+            try{
+                // Query que devuelve los nodos con 6 o mas correos
+                String query = "MATCH (x:User)-[rel:SEND]->(y:User) \n" +
+                        "WHERE rel.quantity >= 6\n" +
+                        "RETURN rel, x, y";
+                final Graph graph = connections.getAsGraph(query, "Six or More");
+
+                SwingUtilities.invokeLater( () -> {
+                    graph.addAttribute("ui.stylesheet",
+                            "node {fill-color: blue; size-mode: dyn-size;} edge {fill-color:grey;}");
+                    Viewer viewer = graph.display();
+                    viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+                });
+
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * Clase thread que obtiene de la db el grafo con los nodos y muestra el PageRank
+     */
+    private class PageRankThread implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                // Query que devuelve toda la DB
+                String query = "MATCH (x:User)-[rel:SEND]->(y:User)\n" +
+                        "RETURN rel, x, y";
+                final Graph graph = connections.getAsGraph(query, "Page Rank Complete Graph");
+
+                SwingUtilities.invokeLater( () -> {
+                    graph.addAttribute("ui.stylesheet",
+                            "node {fill-color: red; size-mode: dyn-size;} edge {fill-color:grey;}");
+                    Viewer viewer = graph.display();
+                    PageRank pageRank = new PageRank();
+                    pageRank.setVerbose(true);
+                    pageRank.init(graph);
+                    for (Node node : graph){
+                        double rank = pageRank.getRank(node);
+                        node.addAttribute("ui.size", 5 + Math.sqrt(graph.getNodeCount() * rank * 20));
+                        node.addAttribute("ui.label", node.getId() + "(" + String.format("%.2f%%", rank * 100) + ")");
+                    }
+                    viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+                });
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Clase thread que  muestra el grafo de la base de datos de neo4j
+     */
+    private class DbToGraphThread implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                // Query que devuelve toda la DB
+                String query = "MATCH (x:User)-[rel:SEND]->(y:User)\n" +
+                        "RETURN rel, x, y";
+                final Graph graph = connections.getAsGraph(query, "Complete Graph");
+
+                SwingUtilities.invokeLater( () -> {
+                    graph.addAttribute("ui.stylesheet",
+                            "node {fill-color: blue; size-mode: dyn-size;} edge {fill-color:grey;}");
+                    Viewer viewer = graph.display();
+                    viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+                });
+            }catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    
+    
+
+
+}
---
+++ MainFrame.java
@@ -0,0 +1,175 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+	private String path;
+    private ArrayList<String[]> matrix;
+
+    /**
+	 * Crea el frame.
+	 */
+	private MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel pnlMain = new JPanel();
+		pnlMain.setBackground(Color.WHITE);
+		contentPane.add(pnlMain, BorderLayout.CENTER);
+		pnlMain.setLayout(null);
+		
+
+        GraphDbManager dbManager = GraphDbManager.getInstance();
+
+		JButton btnComunicacionEntrePersonas = new JButton("Comunicacion");
+		btnComunicacionEntrePersonas.setBounds(141, 287, 140, 25);
+		pnlMain.add(btnComunicacionEntrePersonas);
+        btnComunicacionEntrePersonas.addActionListener(event->{
+			
+		} );
+		
+		JPanel pnlCorreos = new JPanel();
+		pnlCorreos.setBounds(307, 139, 463, 170);
+		pnlMain.add(pnlCorreos);
+		pnlCorreos.setLayout(null);
+		
+		JLabel lblCorreosEnviados = new JLabel("CORREOS ENVIADOS");
+		lblCorreosEnviados.setBounds(156, 12, 150, 15);
+		pnlCorreos.add(lblCorreosEnviados);
+		
+		JPanel pnlSeleccion = new JPanel();
+		pnlSeleccion.setBackground(Color.WHITE);
+		pnlSeleccion.setBounds(141, 39, 180, 85);
+		pnlCorreos.add(pnlSeleccion);
+		pnlSeleccion.setLayout(null);
+		
+		JLabel lblOrigen = new JLabel("Origen");
+		lblOrigen.setBounds(12, 12, 70, 15);
+		pnlSeleccion.add(lblOrigen);
+		
+		JComboBox<String> cmbOrigen = new JComboBox<>();
+		cmbOrigen.setBounds(12, 39, 70, 24);
+		for(int i = 0; i < 14; i++){
+			cmbOrigen.addItem("Per "+ String.valueOf(i + 1));
+		}
+		pnlSeleccion.add(cmbOrigen);
+		
+		JLabel lblDestino = new JLabel("Destino");
+		lblDestino.setBounds(94, 12, 70, 15);
+		pnlSeleccion.add(lblDestino);
+		
+		JComboBox<String> cmbDestino = new JComboBox<>();
+		cmbDestino.setBounds(94, 39, 70, 24);
+		cmbDestino.addItem("-");
+		for(int i = 0; i < 14; i++){
+			cmbDestino.addItem("Per "+ String.valueOf(i + 1));
+		}
+		pnlSeleccion.add(cmbDestino);
+		
+		JButton btnConsultar = new JButton("Consultar");
+		btnConsultar.setBounds(173, 136, 117, 25);
+		btnConsultar.addActionListener(event->{
+				});
+		pnlCorreos.add(btnConsultar);
+		
+		JButton btnMetrica = new JButton("Metrica");
+		btnMetrica.setBounds(12, 287, 117, 25);
+		pnlMain.add(btnMetrica);
+        btnMetrica.addActionListener(event->{
+            dbManager.showPageRank();
+        });
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 324, 758, 121);
+		pnlMain.add(scrollPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(12, 139, 269, 136);
+		pnlMain.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnVisualizarGrafoDe = new JButton("Grafo con relaciones");
+		btnVisualizarGrafoDe.setBounds(12, 25, 245, 25);
+		panel.add(btnVisualizarGrafoDe);
+        btnVisualizarGrafoDe.addActionListener(event->{
+			dbManager.dbToGraph();
+		} );
+		
+		JButton btnRelacionesConMas = new JButton("6 correos o mas");
+		btnRelacionesConMas.setBounds(12, 62, 245, 25);
+		panel.add(btnRelacionesConMas);
+        btnRelacionesConMas.addActionListener(event->{
+            dbManager.sixOrMoreMails();
+        });
+		
+		JButton btnGrafoSimplificado = new JButton("Grafo simplificado");
+		btnGrafoSimplificado.setBounds(12, 99, 245, 25);
+		panel.add(btnGrafoSimplificado);
+        btnGrafoSimplificado.addActionListener(e -> {
+            dbManager.simplifyGraph();
+        });
+		
+		JLabel lblVisual = new JLabel("VISUAL");
+		lblVisual.setBounds(12, 12, 70, 15);
+		panel.add(lblVisual);
+
+        final JButton btnCargarArchivoCsv = new JButton("Cargar archivo CSV a Neo4j");
+        btnCargarArchivoCsv.addActionListener(event-> {
+
+            /* Seleccion de archivos */
+            JFileChooser fc = new JFileChooser();
+            fc.setCurrentDirectory(new java.io.File("./src"));
+            fc.setDialogTitle("Seleccione su archivo");
+            fc.setFileFilter(new FileNameExtensionFilter("CSV files(.csv)", "csv"));
+            if(fc.showOpenDialog(btnCargarArchivoCsv) == JFileChooser.APPROVE_OPTION){
+                path = fc.getSelectedFile().getAbsolutePath();
+
+                try {
+                    Stream<String> stream = Files.lines(Paths.get(path));
+                    matrix = new ArrayList<>();
+                    stream.forEach((string)->
+                            matrix.add(string.split(";"))
+                    );
+
+                    
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+        btnCargarArchivoCsv.setBounds(268, 90, 250, 25);
+        pnlMain.add(btnCargarArchivoCsv);
+	}
+	
+	/**
+	 * Ejecuta la aplicacion.
+	 */
+	public static void main(String[] args) {
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+					frame.setResizable(false);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
